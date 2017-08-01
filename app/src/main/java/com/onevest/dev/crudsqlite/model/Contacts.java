56092b9c513c7c4a29bba148ba6ccdfa72e6307a
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2017-08-01 09:50:17 +0700

    Add Model

---
+++ app/src/main/java/com/onevest/dev/crudsqlite/model/Contacts.java
@@ -0,0 +1,34 @@
+package com.onevest.dev.crudsqlite.model;
+
+public class Contacts {
+    int id;
+    String name;
+    String phone;
+
+    public Contacts() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
